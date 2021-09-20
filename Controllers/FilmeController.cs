e7462f78f0d80ab8325dad9f57b7d772b261a2c4
Samuel <samuel.belo@al.infnet.edu.br>
2021-09-20 16:33:12 -0300

    altera controller e trata notfound

---
+++ Controllers/FilmeController.cs
@@ -0,0 +1,42 @@
+﻿using FilmeAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmeAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+        private static int id = 1;
+        
+        [HttpPost]
+        public IActionResult AdicionaFilme([FromBody] Filme filme)
+        {
+            filmes.Add(filme);
+            filme.Id = id++;
+            return CreatedAtAction(nameof(RecuperaFilmePorId), new { Id = filme.Id}, filme);
+        }
+
+        [HttpGet]
+        public IEnumerable<Filme> RecuperaFilmes()
+        {
+            return filmes;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult RecuperaFilmePorId(int id)
+        {
+            Filme filme = filmes.FirstOrDefault(filme => filme.Id == id);
+            if (filme != null)
+            {
+                return Ok(filme);
+            }
+            return NotFound();
+        }
+    }
+} 
