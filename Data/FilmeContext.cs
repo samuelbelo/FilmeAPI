d6415d2e89ee7c1b61afd97204c18d1e74a30b17
Samuel <samuel.belo@al.infnet.edu.br>
2021-09-22 14:35:18 -0300

    cria camada de acesso a dados

---
+++ Data/FilmeContext.cs
@@ -0,0 +1,19 @@
+﻿using FilmeAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmeAPI.Data
+{
+    public class FilmeContext : DbContext
+    {
+        public FilmeContext(DbContextOptions<FilmeContext> opt) : base (opt)
+        {
+
+        }
+
+        public DbSet<Filme> Filmes { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,7 +1,9 @@
+using FilmeAPI.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -32,6 +34,7 @@ namespace FilmeAPI
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "FilmeAPI", Version = "v1" });
             });
+            services.AddDbContext<FilmeContext>(opts => opts.UseMySQL(Configuration.GetConnectionString("FilmeConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
